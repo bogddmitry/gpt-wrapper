3d3ed96362e60f4468c55a057a2959365b5b0e9c
Dmytro Bohdan <>
2025-05-29 16:43:35 +0300

    CloudFront config

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -138,7 +138,27 @@ resource "aws_cloudfront_distribution" "frontend" {
       origin_access_identity = aws_cloudfront_origin_access_identity.frontend.cloudfront_access_identity_path
     }
   }
-  # ... existing code ...
+  enabled = true
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-frontend"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
 }
 
 resource "aws_cloudfront_origin_access_identity" "frontend" {
